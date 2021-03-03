e34c642ac413c730f28bede9c883f036cfa6cc38
giselebury <54081443+giselebury@users.noreply.github.com>
2021-03-03 00:26:43 -0500

    created sign in page and installed tailwind css

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,27 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        cms
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class= "flex h-screen bg-gray-300">
+    <div class="w-full max-w-xs m-auto bg-gray-100 rounded p-5">
+      <form>
+        <div>
+          <input class="w-full p-2 mb-6 text-gold-400 border-b-2 border-yellow-400 font-bold outline-none focus:bg-gray-300" type="text" name="email" placeholder="email">
+        </div>
+        <div>
+          <input class="w-full p-2 mb-6 text-gold-400 border-b-2 border-yellow-400 font-bold outline-none focus:bg-gray-300" type="text" name="password" placeholder="password">
+        </div>
+        <div>
+          <input class="w-full bg-yellow-400 hover:bg-yellow-300 text-white font-bold py-2 px-4 mb-6 rounded" type="submit">
+        </div>
+      </form>
+      <footer>
+        <ul>
+          <li>
+            <nuxt-link to="/" class="text-gray-400 hover:text-gray-700 font-bold text-sm float-right">Sign Up</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/" class="text-gray-400 hover:text-gray-700 font-bold text-sm float-left">Reset Password</nuxt-link>
+          </li>
+        </ul>
+      </footer>
     </div>
   </div>
 </template>
@@ -37,42 +36,5 @@ export default {}
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
