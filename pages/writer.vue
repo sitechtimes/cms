5b66b75089ee7b2a4989958499a43a50607f7880
giselebury <54081443+giselebury@users.noreply.github.com>
2021-03-08 10:35:08 -0500

    created component for article in writer panel

--- pages/writer.vue
+++ pages/writer.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="bg-gray-50 min-h-screen">
-      <h1 class="text-lg">Your Articles</h1>
+    <div class="flex items-center justify-around">
+      <h1 class="text-3xl font-extrabold text-gray-900">Your Articles</h1>
+      <button
+        type="submit"
+        class="py-2 px-4 border border-transparent rounded-md font-bold text-white bg-yellow-400 hover:bg-yellow-300">
+        Create Article
+      </button>
+    </div>
+    <ArticleHeader />
   </div>
 </template>
 
 <script>
-
-
-export default{
-
-}
+import ArticleHeader from '../components/ArticleHeader';
+export default {
+  components: {
+    ArticleHeader
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
