611b524f834b0d2c29c4d00ac737e7e861466640
giselebury <54081443+giselebury@users.noreply.github.com>
2021-03-08 00:23:44 -0500

    sign up page
    created sign up page, created a component for tech times header, and created a page for writer panel

---
+++ pages/writer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gray-50 min-h-screen">
+      <h1 class="text-lg">Your Articles</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default{
+
+}
+</script>
+
+<style>
+
+</style>
