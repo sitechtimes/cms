5ed7d0a60b84cbd2b11a603e732d616f0a4e56f4
giselebury <54081443+giselebury@users.noreply.github.com>
2021-03-11 01:16:43 -0500

    created navbar component for panel

---
+++ components/PanelNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-gray-700 bg-white border-t border-b body-font">
+    <div class="flex flex-col flex-wrap p-5 mx-auto md:items-center md:flex-row">
+      <div class="flex flex-wrap items-center justify-center text-base ">
+        <nuxt-link to="/writer" class="mr-5 text-sm font-semibold text-gray-600 lg:ml-24 hover:text-gray-800">
+            Your Articles</nuxt-link>
+        <nuxt-link to="/writer" class="mr-5 text-sm font-semibold text-gray-600 lg:ml-24 hover:text-gray-800">
+          Articles In Review</nuxt-link>
+        <nuxt-link to="/writer" class="mr-5 text-sm font-semibold text-gray-600 lg:ml-24 hover:text-gray-800">
+          Ready to be Published</nuxt-link>
+      </div>
+      <div class="flex ml-auto">
+        <button
+          class="items-center px-8 py-2 ml-auto font-semibold transition duration-500 ease-in-out transform rounded-lg text-white bg-yellow-400 hover:bg-yellow-300focus:ring focus:outline-none">
+          Create Article
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PanelNavigation"
+};
+</script>
+
+<style></style>
--- pages/writer.vue
+++ pages/writer.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="bg-gray-50 min-h-screen">
-    <div class="flex items-center justify-around">
-      <h1 class="text-3xl font-extrabold text-gray-900">Your Articles</h1>
-      <button
-        type="submit"
-        class="py-2 px-4 border border-transparent rounded-md font-bold text-white bg-yellow-400 hover:bg-yellow-300">
-        Create Article
-      </button>
-    </div>
+    <PanelNaviation />
     <ArticleHeader />
   </div>
 </template>
 
 <script>
 import ArticleHeader from '../components/ArticleHeader';
+import PanelNavigation from '../components/PanelNavigation';
 export default {
-  components: {
-    ArticleHeader
-  },
+  components: [ArticleHeader, PanelNavigation
+  ],
 };
 </script>
 
