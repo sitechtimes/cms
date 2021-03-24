c467f68912b2d9964fb385dec0ca3478e56fc62e
giselebury <54081443+giselebury@users.noreply.github.com>
2021-03-24 10:01:17 -0400

    created page for profile

---
+++ pages/profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-50 min-h-screen">
+      <h3 class="text-sm font-medium text-gray-900">My Account</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    layout: 'siteHeader',
+}
+</script>
+
+<style>
+
+</style>
