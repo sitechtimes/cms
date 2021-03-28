bc77a4609d229c40a2eaf23f118aabbcd98e37bc
edis <edis@gonuler.com>
2021-03-28 00:14:31 -0400

    extend background to the whole page and create new article btn

---
+++ components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center items-center bg-gray-50">
+      <header>
+        <h1 class="text-6xl mt-6 text-center font-extrabold text-gray-900">
+              The Tech Times</h1>
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
+  <div>
     <Nuxt />
+  </div>
 </template>
 
 <script>
