db79581a67f209b6dac3b5d5f270d70137d7e320
edis <edis@gonuler.com>
2021-04-16 09:58:25 -0400

    add sendToReview btn and alert

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -20,6 +20,8 @@
         <span class="hidden sm:block ml-3">
         <button @click="preview = !preview" type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
 
+
+
         <svg class="-ml-1 mr-2 h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
           <path v-show="preview" d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
           <path v-show="!preview" fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd" />
@@ -31,14 +33,14 @@
         <span class="sm:ml-3">
 
       <button @click="saveArticle" type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-        <!-- Heroicon name: solid/check -->
-        <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 -ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
         </svg>
         Save Article
       </button>
 
     </span>
+
       </div>
     </div>
 
@@ -46,6 +48,16 @@
 
       <div class="preview-content" v-show="preview" v-html="article.content"></div>
 
+      <div class="flex justify-center py-4">
+        <button @click="reviewModel = !reviewModel" type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+          </svg>
+          Send to Review
+        </button>
+      </div>
+
+      <SendToReviewModel v-if="reviewModel" @dismissModelAlert="dismissModelAlert" />
     </div>
 
   </div>
@@ -56,15 +68,17 @@
   import { VueEditor } from "vue2-editor";
   import SuccessAlert from "../../../components/alerts/SuccessAlert";
   import ErrorMessage from "../../../components/ErrorMessage";
+  import SendToReviewModel from "../../../components/alerts/SendToReviewAlert";
 
   export default {
     layout: 'dashboard',
     components: {
-      VueEditor, SuccessAlert, ErrorMessage
+      VueEditor, SuccessAlert, ErrorMessage, SendToReviewModel
     },
   data () {
     return {
       preview: false,
+      reviewModel: false,
 
       success: null,
       errors: null,
@@ -112,6 +126,9 @@
       },
       dismissAlert(){
         this.success = null
+      },
+      dismissModelAlert(){
+        this.reviewModel = false
       }
     }
 }
