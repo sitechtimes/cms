c099513219707fe6b981f7bcf9335cb7f359978d
edis <edis@gonuler.com>
2021-05-10 17:34:18 -0400

    add options dropdown

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -31,26 +31,29 @@
       </button>
     </span>
 
-            <span class="sm:ml-3">
+                <span class="sm:ml-3">
+            <div class="relative inline-block text-left">
+              <div>
+                <button type="button"  @click="open = !open" class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                  Options
+                  <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                  </svg>
+                </button>
+              </div>
 
-      <button @click="saveArticle" type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 -ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
-        </svg>
-        Save Article
-      </button>
-
-    </span>
-
-            <span class="sm:ml-3">
-      <button @click="deleteModel = !deleteModel" type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white  hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 bg-red-600">
-        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-        </svg>
-      </button>
-    </span>
-
-
+                <div v-if="open" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none py-1" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+<!--                  <div class="pt-1">-->
+                  <a @click="saveArticle" class="cursor-pointer text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:bg-indigo-600 hover:text-white" role="menuitem" tabindex="-1" id="menu-item-0">Save Article</a>
+<!--                    </div>-->
+<!--                 <div class="py-1">-->
+                  <a @click="reviewModel = !reviewModel" class="cursor-pointer text-gray-700 block px-4 py-2 text-sm hover:bg-indigo-600 hover:text-white" role="menuitem" tabindex="-1" id="menu-item-1">Send To Review</a>
+<!--                </div>-->
+                  <a @click="deleteModel = !deleteModel" class="cursor-pointer text-red-700 block px-4 py-2 text-sm hover:bg-red-100 " role="menuitem" tabindex="-1" id="menu-item-2">Delete</a>
+<!--                  </div>-->
+              </div>
+            </div>
+                </span>
 
           </div>
         </div>
@@ -63,14 +66,6 @@
 
         <div class="preview-content" v-show="preview" v-html="article.content"></div>
 
-        <div class="flex justify-center py-4">
-          <button @click="reviewModel = !reviewModel" type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-            </svg>
-            Send to Review
-          </button>
-        </div>
 
         <WarningAlert
           v-if="reviewModel"
@@ -233,6 +228,7 @@
       sendToDraftModel: false,
       sendToAdminModel: false,
       publishedModel: false,
+      open: false,
 
       success: null,
       errors: null,
