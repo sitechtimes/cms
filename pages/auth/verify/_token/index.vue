e0bc1f626049984c7cca58dddf0d7ded9e422cd8
Edis Gonuler <edis@gonuler.com>
2021-05-21 12:03:15 -0400

    remove mounted

---
+++ pages/auth/verify/_token/index.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+  export default {
+    async mounted() {
+      console.log("hello")
+      // const resp = await this.$axios.get(`/auth/verify/${this.$route.params.token}`);
+      // this.$auth.setUser(resp.data);
+      //
+      // if (this.$auth.user.verified){
+      //   await this.$router.push('/');
+      // }else{
+      //   await this.$router.push('/auth/signup');
+      // }
+    }
+  }
+</script>
