dd2fe53e6ce1a7e2178b733e6f814250b68af42a
Edis Gonuler <edis@gonuler.com>
2021-05-21 12:29:12 -0400

    add _token page

---
+++ pages/auth/verify/_token.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    try {
+      const resp = await this.$axios.get(`/auth/verify/${this.$route.params.token}`);
+      this.$auth.setUser(resp.data);
+
+      if (this.$auth.user.verified) {
+        await this.$router.push('/');
+      } else {
+        await this.$router.push('/auth/signup');
+      }
+    } catch(e){
+
+    }
+  }
+}
+</script>
