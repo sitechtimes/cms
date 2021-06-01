ad10093af4db8591abb0903f1eb92ad6349997d9
Melvin Aziz <58566746+MelvAziz@users.noreply.github.com>
2021-05-31 21:52:15 -0400

    I think I fixed an issue?
    I had trouble getting the files to load, however testing the code in general leads me to believe this should fix the the bar for Options clipping off screen on mobile devices. I'll have to check later when I get the code working for sure, however this should be fine for now)

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -42,7 +42,7 @@
                           class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
                           id="menu-button" aria-expanded="true" aria-haspopup="true">
                     Options
-                    <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                    <svg class="-mr-1 ml-2 h-5 w-5 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                          fill="currentColor" aria-hidden="true">
                       <path fill-rule="evenodd"
                             d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -52,7 +52,7 @@
                 </div>
 
                 <div v-if="open"
-                     class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none py-1"
+                     class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none py-1 ml-sm-5"
                      role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                   <a @click="saveArticle"
                      class="cursor-pointer text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:bg-indigo-600 hover:text-white"
