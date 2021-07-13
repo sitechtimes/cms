8eb9d06eedc99aa5ad02a3c7b242e56cbe332a88
Edis Gonuler <edis@gonuler.com>
2021-07-13 06:34:58 -0400

    fix display issues of text and make sections responsive

--- pages/articles/_id/index.vue
+++ pages/articles/_id/index.vue
@@ -13,13 +13,13 @@
             <div class="flex-1 min-w-0">
               <input type="text"
                      v-model="article.title"
-                     class="text-2xl font-bold leading-7 background-blue sm:text-3xl sm:truncate inline-block
+                     class="text-3xl font-bold leading-7 background-blue sm:text-3xl sm:truncate inline-block
                focus:outline-none focus:ring focus:border-blue-300 bg-gray-100"/>
             </div>
 
             <div class="mt-5 flex lg:mt-0 lg:ml-4">
 
-        <span class="hidden sm:block ml-3">
+        <span class="mr-3 md:mr-0">
         <button @click="preview = !preview" type="button"
                 class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
 
@@ -103,16 +103,16 @@
         </div>
 
         <div v-show="article.status === 'review'">
-          <div class="lg:flex lg:items-center lg:justify-between py-6">
-            <div class="flex-1 min-w-0">
-              <h1 class="text-2xl font-bold leading-7 background-blue sm:text-3xl sm:truncate inline-block
+           <div>
+             <div class="flex-1 min-w-0">
+               <h1 class="text-3xl font-bold leading-9 background-blue
                focus:outline-none focus:ring focus:border-blue-300 bg-gray-100">{{ article.title }}</h1>
             </div>
 
-            <div class="mt-5 flex lg:mt-0 lg:ml-4"
+            <div class="py-6 flex"
                  v-if="['editor', 'admin'].includes(this.$auth.user.role)">
 
-                <span class="hidden sm:block ml-3">
+                <span class="mr-3">
                   <button @click="sendToDraftModel = !sendToDraftModel" type="button"
                           class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     <svg class="-ml-1 mr-2 h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -175,16 +175,16 @@
         />
 
       <div v-show="article.status === 'ready'">
-        <div class="lg:flex lg:items-center lg:justify-between py-6">
+        <div>
           <div class="flex-1 min-w-0">
-            <h1 class="text-2xl font-bold leading-7 background-blue sm:text-3xl sm:truncate inline-block
+            <h1 class="text-3xl font-bold leading-9 background-blue
                focus:outline-none focus:ring focus:border-blue-300 bg-gray-100">{{ article.title }}</h1>
           </div>
 
-          <div class="mt-5 flex lg:mt-0 lg:ml-4"
+          <div class="py-6 flex"
                v-if="this.$auth.user.role === 'admin'">
 
-                <span class="hidden sm:block ml-3">
+                <span class="mr-3">
 
                   <button @click="sendToDraftModel = !sendToDraftModel" type="button"
                           class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
