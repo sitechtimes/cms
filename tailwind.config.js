a854b4eff6651affdae7de3aafe22ea38c33673f
Ehan <71458581+ethanl66@users.noreply.github.com>
2021-07-21 12:43:05 -0400

    beginning of tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,31 @@
 module.exports = {
   purge: [
-    './components/**/*.{vue,js}',
-     './layouts/**/*.vue',
-     './pages/**/*.vue',
-     './plugins/**/*.{js,ts}',
-     './nuxt.config.{js,ts}',
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: {
+          DEFAULT: "#d9b75e",
+          light: "#fff3d2"
+        },
+        offWhite: "#fffaf4",
+        midGray: "#636363"
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active"]
+    }
   },
   plugins: [
     //require('@tailwindcss/forms'),
   ],
   important: true
-}
+};
