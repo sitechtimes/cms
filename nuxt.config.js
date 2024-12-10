a8eb08e48b838872b7fb923ef7f54e7a8b16237d
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-12-10 13:02:41 -0500

    eek

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,26 @@
-import { ENV_VALUES } from './config';
+import { ENV_VALUES } from "./config";
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   ssr: false,
-  target: 'static',
+  target: "static",
   head: {
-    title: 'Tech Times CMS',
+    title: "Tech Times CMS",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/vuex-persist', ssr: false }
-  ],
+  plugins: [{ src: "~/plugins/vuex-persist", ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -33,18 +28,14 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/auth'
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+  build: {},
 
   // change default port to 8000
   server: {
@@ -52,7 +43,8 @@ export default {
   },
 
   axios: {
-    baseURL: ENV_VALUES[process.env.ENV].api
+    // baseURL: ENV_VALUES[process.env.ENV].api
+    baseURL: "http://localhost:3000/"
   },
 
   auth: {
@@ -60,22 +52,22 @@ export default {
       local: {
         autoFetchUser: false,
         endpoints: {
-          login: { url: '/auth/signin', method: 'post', propertyName: 'token' },
+          login: { url: "/auth/signin", method: "post", propertyName: "token" },
           user: false,
-          logout: false,
+          logout: false
         },
 
-        tokenType: '',
-      },
+        tokenType: ""
+      }
     },
     redirect: {
       login: false,
-      logout: '/auth/signin',
-      home: false,
+      logout: "/auth/signin",
+      home: false
     }
   },
 
   generate: {
     fallback: true
   }
-}
+};
