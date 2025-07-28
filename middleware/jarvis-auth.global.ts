5acccb6c20463b6ee195f37b945c3c02c89fe879
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-07-28 11:54:54 -0400

    FIXED IT HEHEHEHHE
    FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED FIXED

--- middleware/jarvis-auth.global.ts
+++ middleware/jarvis-auth.global.ts
@@ -1,16 +1,10 @@
 export default defineNuxtRouteMiddleware((to) => {
-  // const nuxtApp = useNuxtApp()
-  const userStore = useUserStore()
-
-  console.log(JSON.stringify(userStore.user))
-  console.log(userStore.user)
-
   // don't run on server because locaalstorage
   // https://nuxt.com/docs/3.x/guide/directory-structure/middleware#when-middleware-runs
   if (import.meta.server) return
 
-  console.log(!to.path.startsWith('/auth'))
-  console.log(!userStore.user)
+  const userStore = useUserStore()
+
   if (!to.path.startsWith('/auth') && !userStore.user)
     return navigateTo('/auth/signin')
 })
