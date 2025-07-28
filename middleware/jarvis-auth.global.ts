257a9ed1b2d666fd98cdc12ed56b7287cb115708
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-07-28 11:55:42 -0400

    I AM NOT JARVIS LI
    JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI JARVIS LI

--- middleware/jarvis-auth.global.ts
+++ middleware/jarvis-auth.global.ts
@@ -3,6 +3,9 @@ export default defineNuxtRouteMiddleware((to) => {
   // https://nuxt.com/docs/3.x/guide/directory-structure/middleware#when-middleware-runs
   if (import.meta.server) return
 
+  const isJarvis = false
+  if (isJarvis) return // GET OUT
+
   const userStore = useUserStore()
 
   if (!to.path.startsWith('/auth') && !userStore.user)
