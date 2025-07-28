0422fd7a96fa05f55b2b256073600f472aca8a59
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-07-28 09:38:12 -0400

    fixed dropdown

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -14,13 +14,13 @@
       <div class="flex-none">
         <details
           v-if="user"
-          class="du-dropdown du-dropdown-bottom du-dropdown-left"
+          class="du-dropdown du-dropdown-bottom du-dropdown-end"
         >
           <summary class="du-avatar w-10 cursor-pointer">
             <img class="rounded-full" :src="user.imageUrl" alt="" />
           </summary>
           <ul
-            class="du-menu du-dropdown-content bg-base-100 du-rounded-box z-1 w-52 p-2 shadow-sm"
+            class="du-menu du-dropdown-content bg-base-100 du-rounded-box z-1 mt-1 w-52 p-2 shadow-sm"
           >
             <li><NuxtLink to="/profile">Your Profile</NuxtLink></li>
             <li><NuxtLink to="/users">Users</NuxtLink></li>
