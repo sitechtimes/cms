4e1ff6f3ad45775086ef3acdc19dd31f97a5773c
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-07-29 08:11:33 -0400

    NO MORE SHADOW

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="bg-gray-800">
-    <nav class="du-navbar mx-auto max-w-3xl px-5 shadow-sm md:max-w-7xl">
+    <nav class="du-navbar mx-auto max-w-3xl px-5 md:max-w-7xl">
       <div class="">
         <img class="mx-auto h-auto w-10" src="~/assets/img/logo.svg" alt="" />
       </div>
