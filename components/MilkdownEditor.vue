d0804f74ffb48c521fe7c62f5f99854395f19715
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-08-05 11:25:34 -0400

    make milkdown work with crepe
    MILKITY MILK MILK MILK MILK

--- components/MilkdownEditor.vue
+++ components/MilkdownEditor.vue
@@ -4,20 +4,18 @@
 
 <script setup lang="ts">
 import { Milkdown, useEditor } from '@milkdown/vue'
+import { Crepe } from '@milkdown/crepe'
 import '@milkdown/crepe/theme/common/style.css'
 import '@milkdown/crepe/theme/frame.css'
-
-const markdown = `# Milkdown Nuxt Commonmark
-
-> You're scared of a world where you're needed.
-
-This is a demo for using Milkdown with **Nuxt**.`
-
-import { Crepe } from '@milkdown/crepe'
 
 useEditor((root) => {
   const crepe = new Crepe({
     root,
+    featureConfigs: {
+      [Crepe.Feature.Cursor]: {
+        virtual: false,
+      },
+    },
   })
 
   return crepe
---
+++ pages/articles/edit/[id].vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="mx-auto max-w-3xl py-8 md:max-w-7xl">
+    <div class="flex justify-between">
+      <h1 class="text-3xl font-bold text-gray-900">Edit Article</h1>
+      <div>
+        <button class="du-btn">
+          <Icon name="heroicons:link-16-solid" /> View
+        </button>
+        <div class="du-dropdown du-dropdown-end">
+          <div tabindex="0" role="button" class="du-btn m-1">Options</div>
+          <ul
+            tabindex="0"
+            class="du-dropdown-content du-menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="max-w-7xl py-4">
+      <label class="text-md block font-medium text-gray-700"> Title </label>
+      <div class="mt-1 flex w-80 rounded-md shadow-sm md:w-100 lg:w-120">
+        <input
+          type="text"
+          class="du-input text-md block flex-1 rounded border border-gray-300 px-3 py-3"
+        />
+      </div>
+    </div>
+    <div class="max-w-7xl py-4">
+      <label class="text-md block font-medium text-gray-700">
+        Custom Author
+      </label>
+      <div class="mt-1 flex w-80 rounded-md shadow-sm md:w-100 lg:w-120">
+        <input
+          type="text"
+          class="du-input text-md block flex-1 rounded border border-gray-300 px-3 py-3"
+        />
+      </div>
+    </div>
+    <div class="max-w-7xl py-4">
+      <label class="text-md block font-medium text-gray-700"> Category </label>
+      <select class="du-select mt-1 capitalize shadow">
+        <option v-for="topic in topics">
+          {{ topic }}
+        </option>
+      </select>
+    </div>
+    <div class="max-w-7xl py-4">
+      <label class="text-md block font-medium text-gray-700"> Category </label>
+      <div class="mt-1 rounded border border-gray-300 shadow">
+        <MilkdownProvider>
+          <MilkdownEditor />
+        </MilkdownProvider>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MilkdownProvider } from '@milkdown/vue'
+
+definePageMeta({
+  layout: 'navbar',
+})
+
+const topics = [
+  'feature',
+  'news',
+  'school',
+  'entertainment',
+  'lifestyle',
+  'opinion',
+  'science',
+  'technology',
+] as const
+</script>
