8838e5495dfc443a67eac6e13cf157d7666c5943
kongeebowl <kadenpang09@gmail.com>
2025-08-13 09:06:22 -0400

    e
    Co-Authored-By: Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>

---
+++ components/Article/Article.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+const article = ref<Article>()
+const route = useRoute()
+
+onBeforeMount(async () => {
+  article.value = await requestEndpoint<Article>(
+    `/cms/${route.params.id}`,
+    'GET'
+  )
+})
+</script>
--- components/Dashboard/MyArticles.vue
+++ components/Dashboard/MyArticles.vue
@@ -1,6 +1,9 @@
 <template>
   <div v-for="status in statuses" :key="status" class="px-4 pt-2">
-    <DashboardTable v-if="articleStore.sortArticles(status).length > 0" :title="status">
+    <DashboardTable
+      v-if="articleStore.sortArticles(status).length > 0"
+      :title="status"
+    >
       <DashboardArticleRows
         v-for="article in articleStore.sortArticles(status)"
         :key="article._id"
@@ -12,8 +15,8 @@
       v-if="articleStore.sortArticles(status).length === 0"
       class="mx-auto flex max-w-7xl flex-col gap-4 p-4"
     >
-      <h2 class="text-l px-2 font-bold capitalize">{{ status }}</h2>
-      <p>
+      <h2 class="px-2 text-xl font-bold capitalize">{{ status }}</h2>
+      <p class="pl-2">
         You have no articles in
         <span class="capitalize">{{ status }}</span
         >.
