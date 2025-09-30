aa8b426e642a1c5ffed81ec609d1132f42d3d3dc
kongeebowl <kadenpang09@gmail.com>
2025-09-30 10:58:04 -0400

    I HATE THIS GODFORSAKEN WEBSITE

--- utils/requestEndpoint.ts
+++ utils/requestEndpoint.ts
@@ -31,6 +31,8 @@ export async function requestEndpoint<T>(
   const config = useRuntimeConfig()
   const userStore = useUserStore()
 
+  console.log(67)
+
   const headers: HeadersInit = {}
 
   if (userStore.user)
@@ -44,14 +46,20 @@ export async function requestEndpoint<T>(
     headers['Content-Type'] = 'application/json'
   }
 
+  console.log(68)
+
   options.headers = headers
-
+  console.log(68.1)
   const res = await fetch(config.public.backend + endpoint, options)
-
+  console.log(68.2)
   const contentLength = res.headers.get('Content-Length')
+  console.log(68.3)
   if (contentLength === '0') return undefined as T
+  console.log(res)
 
   const jason = await res.json()
+
+  console.log(69)
 
   if (jason.message === 'you are invalid') return userStore.signOut()
 
@@ -64,6 +72,8 @@ export async function requestEndpoint<T>(
     console.error(new Error(jason.message))
     throw { ...jason, status: res.status }
   }
+
+  console.log(41)
 
   return jason
 }
