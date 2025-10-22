d282d2ef0758ce7c77b58ff4a96724c6adee2c47
kongeebowl <kadenpang09@gmail.com>
2025-10-22 11:03:09 -0400

    fIX SOon

--- components/Dashboard/ArticleRows.vue
+++ components/Dashboard/ArticleRows.vue
@@ -1,9 +1,9 @@
 <template>
-  <tr class="h-12">
+  <tr v-if="current" class="h-12">
     <td
       class="w-fit min-w-40 overflow-hidden text-sm font-medium text-ellipsis whitespace-nowrap text-gray-900"
     >
-      {{ article.title }}
+      {{ current.title }}
     </td>
 
     <td class="w-60 min-w-40 text-sm text-gray-900">
@@ -14,22 +14,31 @@
       <span
         class="inline-flex rounded-full bg-green-100 px-2 py-1 text-xs font-semibold text-green-800 capitalize"
       >
-        {{ article.status }}
+        {{ current.status }}
       </span>
     </td>
 
     <td class="w-40 min-w-20 text-right text-sm font-medium whitespace-nowrap">
       <NuxtLink
-        v-if="article.status === 'draft'"
+        v-if="article && article.status === 'draft'"
         class="text-indigo-600 hover:text-indigo-900"
         :to="`/articles/edit/${article._id}`"
       >
         Edit
       </NuxtLink>
       <NuxtLink
-        v-if="article.status === 'review' || article.status === 'ready'"
+        v-if="
+          article && [article.status === 'review' || article.status === 'ready']
+        "
         class="text-indigo-600 hover:text-indigo-900"
         :to="`/articles/${article._id}`"
+      >
+        View
+      </NuxtLink>
+      <NuxtLink
+        v-if="publishedArticle"
+        class="text-indigo-600 hover:text-indigo-900"
+        :to="`/articles/view/${publishedArticle.slug}`"
       >
         View
       </NuxtLink>
@@ -38,18 +47,26 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ article: Article }>()
+const props = defineProps<{
+  article?: Article
+  publishedArticle?: PublishedArticle
+}>()
+
+const current = computed(() => props.article || props.publishedArticle)
 
 const formatDate = computed(() => {
-  const date = new Date(props.article.updatedAt)
+  const updatedAt =
+    props.article?.updatedAt || props.publishedArticle?.updatedAt
 
-  const formattedDate = date.toLocaleString('en', {
+  if (!updatedAt) return ''
+
+  const date = new Date(updatedAt)
+
+  return date.toLocaleString('en', {
     month: 'short',
     weekday: 'short',
     day: 'numeric',
     year: 'numeric',
   })
-
-  return formattedDate
 })
 </script>
---
+++ pages/articles/view/[slug].vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <main v-if="article"></main>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'navbar',
+})
+
+const article = ref<PublishedArticle>()
+const route = useRoute()
+
+onBeforeMount(async () => {
+  article.value = await requestEndpoint<PublishedArticle>(
+    `/articles/${route.params.slug}`,
+    'GET'
+  )
+})
+</script>
