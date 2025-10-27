d841460f9e8bb238622d4695af1e24fa7ea8b26a
kongeebowl <kadenpang09@gmail.com>
2025-10-27 10:50:01 -0400

    i did what needed to be done now i will pass on any other ublished issues to the next victim (probably me) 😭

--- pages/articles/view/[slug].vue
+++ pages/articles/view/[slug].vue
@@ -1,50 +1,7 @@
 <template>
   <div>
     <main v-if="article">
-      <div class="mx-auto max-w-3xl py-8 md:max-w-7xl">
-        <div
-          class="du-badge du-badge-lg cursor-default text-white capitalize transition duration-200 ease-in-out hover:-translate-y-2"
-          :class="[
-            article?.category
-              ? categoryMap[article.category as keyof typeof categoryMap]
-              : '',
-          ]"
-        >
-          {{ article?.category }}
-        </div>
-        <article class="prose max-w-[56%]">
-          <div
-            ref="titleRef"
-            class="m-auto mb-3.5 text-5xl font-semibold"
-          ></div>
-          <div v-if="article" class="my-8">
-            <img
-              v-if="article.imageUrl"
-              :src="article.imageUrl"
-              alt=""
-              class="w-3xl rounded-2xl"
-            />
-            <div>
-              <span class="mr-6">
-                <Icon
-                  name="heroicons:user-16-solid"
-                  class="relative top-0.5 h-5 w-5"
-                />
-                {{ article.customAuthor }}
-              </span>
-              <span>
-                <Icon
-                  name="heroicons:clock-16-solid"
-                  class="relative top-0.5 h-5 w-5"
-                />
-                {{ formatDate }}
-              </span>
-            </div>
-          </div>
-
-          <div ref="contentRef"></div>
-        </article>
-      </div>
+      <PublishedArticleComponent :article="article" />
     </main>
   </div>
 </template>
@@ -56,53 +13,9 @@ definePageMeta({
 
 const article = ref<PublishedArticle>()
 const route = useRoute()
-const titleRef = useTemplateRef('titleRef')
-const contentRef = useTemplateRef('contentRef')
 
-console.log(titleRef.value)
-console.log(contentRef.value)
-
-const categoryMap = {
-  news: 'bg-[var(--news)]',
-  feature: 'bg-[var(--feature)]',
-  entertainment: 'bg-[var(--entertainment)]',
-  lifestyle: 'bg-[var(--lifestyle)]',
-  opinion: 'bg-[var(--opinion)]',
-  science: 'bg-[var(--science)]',
-  technology: 'bg-[var(--technology)]',
-  school: 'bg-[var(--school)]',
-} as const
-
-const options: Intl.DateTimeFormatOptions = {
-  weekday: 'short',
-  year: 'numeric',
-  month: 'short',
-  day: 'numeric',
-}
-const formatDate = computed(() => {
-  const date = new Date(article.value?.updatedAt)
-
-  const formattedDate = date.toLocaleDateString('en-US', options)
-  return formattedDate
-})
-
-onMounted(async () => {
-  article.value = await requestEndpoint<PublishedArticle>(
-    `/articles/${route.params.slug}`,
-    'GET'
-  )
-  await nextTick()
-  console.log(article.value)
-  if (article.value) {
-    if (contentRef.value) {
-      contentRef.value.innerHTML = article.value.content
-      console.log(contentRef.value)
-    }
-
-    if (titleRef.value) {
-      titleRef.value.innerHTML = article.value.title
-      console.log(titleRef.value)
-    }
-  }
-})
+article.value = await requestEndpoint<PublishedArticle>(
+  `/articles/${route.params.slug}`,
+  'GET'
+)
 </script>
