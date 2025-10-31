6ddebcfe36aa0a90bceb33f9d4d6df5d67fd78dd
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-10-30 21:24:40 -0400

    Update ToastEditor.vue

--- components/ToastEditor.vue
+++ components/ToastEditor.vue
@@ -67,6 +67,7 @@ const initializeEditor = async () => {
     // Update model on content change
     editor.on('change', () => {
       html.value = editor.getHTML()
+      console.log(html.value)
     })
 
     isEditorReady.value = true
