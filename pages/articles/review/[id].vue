470f138c2a6fccdeb2ba20348e501d2e5f636129
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-12-09 10:57:19 -0500

    dfoneoju

--- pages/articles/review/[id].vue
+++ pages/articles/review/[id].vue
@@ -31,7 +31,7 @@
                         class="du-menu du-dropdown-content bg-base-100 du-rounded-box z-1 w-52 p-2 shadow-sm"
                     >
                         <li @click="confirmSend">
-                        <button>Send to Ready</button>
+                        <button> Send to Ready </button>
                         </li>
                     </ul>
                     </details>
@@ -39,22 +39,34 @@
             </div>
             <div v-if="article.editorResponses.length != 0" class="max-w-7xl py-4">
                 <label class="text-md block font-medium text-gray-700"> Editor Notes </label>
-                <div class="mt-1 flex flex-col w-80 rounded-md shadow-md md:w-100 lg:w-120">
-                    <h3 class="text-md flex-1 px-3 py-3" v-for="response in article.editorResponses" :key="response.name">
-                        {{ response.name }}: {{ response.text }}
+                <div class="mt-1 flex flex-col w-140 rounded-md shadow-md md:w-160 lg:w-180 flex-wrap">
+                    <h3 class="flex justify-between items-start gap-2 px-3 py-3 whitespace-normal break-all"
+                        v-for="response in article.editorResponses" 
+                        :key="response.name"
+                        >
+                        <span class="flex-1 break-all">
+                            {{ response.name }}: {{ response.text }}
+                        </span>
+
+                        <button 
+                            class="shrink-0 pl-2 text-gray-500 hover:text-red-500 font-bold text-2xl leading-none"
+                            @click="deleteNotes(response)"
+                        >
+                            <Icon name="heroicons:x-mark-16-solid" />                            
+                        </button>
                     </h3>
                 </div>
             </div>
             <div class="max-w-7xl py-4">
                 <label class="text-md block font-medium text-gray-700"> Add Editor Notes </label>
-                <div class="mt-1 flex w-80 rounded-md shadow-sm md:w-100 lg:w-120">
+                <div class="mt-1 flex w-80 rounded-md md:w-100 lg:w-120">
                     <input
                     v-model="newNote"
                     type="text"
-                    class="du-input text-md block flex-1 rounded border border-gray-300 px-3 py-3"
+                    class="du-input text-md block flex-1 shadow-sm rounded border border-gray-300 pl-3 py-3"
                     />
+                    <button @click="saveArticle" class="ml-2 du-btn du-btn-success shadow-sm"> Send </button>
                 </div>
-                <button> Send </button>
             </div>
             <ArticleComponent :article="article" />
             
@@ -67,8 +79,11 @@ definePageMeta({
     layout: 'navbar',
 })
 
-const article = ref<Article>()
 const route = useRoute()
+
+const userStore = useUserStore()
+
+const article = ref<Article>()
 const newNote = ref<string>('')
 
 const dropdown = useTemplateRef('dropdown')
@@ -81,6 +96,24 @@ function closeDropdown() {
 function confirmSend() {
   closeDropdown()
   modal.value?.showModal()
+}
+
+function deleteNotes(response: { name: string; text: string }) {
+  const index = article.value?.editorResponses.findIndex((note) => note === response)
+  if (!index || index === -1) return
+  article.value?.editorResponses.splice(index, 1)
+  requestEndpoint(`/cms/${route.params.id}`, 'PUT', article.value)
+}
+
+async function saveArticle() {
+    if (newNote.value.trim() !== '') {
+        article.value?.editorResponses.push({
+        name: userStore.user?.name || 'Anonymous',
+        text: newNote.value.trim(),
+        })
+        newNote.value = ''
+    }
+  requestEndpoint(`/cms/${route.params.id}`, 'PUT', article.value)
 }
 
 function sendToReady() {
@@ -96,7 +129,7 @@ onBeforeMount(async () => {
         `/cms/${route.params.id}`,
         'GET'
     )
-    
+
     console.log('Fetched article:')
     console.log(article.value)
 })
